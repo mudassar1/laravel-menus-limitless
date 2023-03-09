ef03fbbd31b3505fc52889ce09b333aa2bd463f9
mudassar1 <mudassaryaseensajid@hotmail.com>
2023-03-09 23:47:13 +0500

    auto active parent menu

--- views/item/dropdown.blade.php
+++ views/item/dropdown.blade.php
@@ -1,15 +1,15 @@
-<li class="nav-item nav-item-submenu {{ $item->hasActiveOnChild() ? 'active' : '' }}">
-  <a href="#" class="nav-link">
+<li class="nav-item nav-item-submenu {{ $item->hasActiveOnChild() ? 'nav-item-open' : '' }}">
+  <a href="#" class="nav-link {{ $item->hasActiveOnChild() ? 'active' : '' }}">
     {{ $item->title }}
     <span class="caret"></span>
   </a>
-  <ul class="nav-group-sub collapse">
+  <ul class="nav-group-sub collapse {{ $item->hasActiveOnChild() ? 'show' : '' }}">
     @foreach ($item->childs as $child)
-    	@if ($child->hasChilds())
-  			@include('menus::item.dropdown', ['item' => $child])
-    	@else
-  			@include('menus::item.item', ['item' => $child])
-    	@endif
+      @if ($child->hasChilds())
+        @include('menus::item.dropdown', ['item' => $child])
+      @else
+        @include('menus::item.item', ['item' => $child])
+      @endif
     @endforeach
   </ul>
 </li>
